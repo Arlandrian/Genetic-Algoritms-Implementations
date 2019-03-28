5b352e3b9af7b711490eb7f49f00fa264c326e19
Arlandrian <alo_istanbul@hotmail.com>
2019-03-28 22:57:04 +0300

    completed

--- Genetic Snake/genetic.py
+++ Genetic Snake/genetic.py
@@ -1,32 +1,72 @@
+import random
 
 class Individual:
     #individuals of population
     #Paths for our problem
+    def __init__(self,gene_length,random,path):
+        self.fitness = -1
+        if random == True:
+            self.path = self.generatePathRandom(gene_length)
+        else:
+            self.path = path
 
-    def __init__(self,genes):
-        assert(len(genes) > 3)
-        self.genes = genes
-        self.__reset_params()
+    def generatePathRandom(self,gene_length):
+        path = []
+        for _ in range(gene_length):
+            path.append( random.randint(1,4) )
+
+        return path
 
     def swap(self,g1,g2):
-        #swaps two gene for individual
-        self.genes[0]
-        a, b = self.genes.index(g1),self.genes.index(g2)
-        self.genes[b], self.genes[a] = self.genes[a], self.genes[b]
-        self.__reset_params()
-
-    def __reset_params(self):
-        self.__travel_cost = 0
-        self.__fitness = 0
+        self.path[0]
+        a, b = self.path.index(g1), self.path.index(g2)
+        self.path[b], self.path[a] = self.path[a], self.path[b]
 
 
-class population:
+    def setFitness(self,fitness):
+        self.fitness = fitness
+    
+    def toString(self):
+        
+        result = "Fitness: %f" %self.fitness
+        result +="\nPath: "
+        for i in range(len(self.path)):
+            result+= str(self.path[i])
+            result+= " "
+        
+        return result
+
+class Population:
     #Population of individuals
-    def __init__(self,individuals):
-        self.individuals = individuals
+    def __init__(self,population_size,gene_length,is_random,individuals):
+        self.population_size = population_size
+        if is_random:
+            self.individuals = []
+            for _ in range(population_size):
+                self.individuals.append( Individual(gene_length,True,[]) )
+        else:
+            self.individuals = individuals
+    
+    def addIndividual(self,individual):
+        self.individuals.append(individual)
 
-    @staticmethod
-    def gen_individuals(size,genes):
-        individuals =[size]
-        for _ in range(size):
-            individuals.append()
+    def removeIndividual(self,individual):
+        self.individuals.remove(individual)
+
+    def getIndividualCount(self):
+        return len(self.individuals)
+
+    def getFittest(self):
+        fittest = self.individuals[0]
+        for path in self.individuals:
+            if path.fitness > fittest.fitness:
+                fittest = path
+        
+        return fittest
+    
+    def calculateAverageFitness(self):
+        total = 0
+        for ind in self.individuals:
+            total += ind.fitness
+
+        return total / self.population_size
--- Genetic Snake/main.py
+++ Genetic Snake/main.py
@@ -1,76 +1,154 @@
 import argparse
 from datetime import datetime
-import random
+from random import random,sample,randint
 import numpy as np
 import time
+import copy
+import random
+import math
+
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
+from genetic import Individual,Population
 
 #total generations
 #average fitness
 #total population
 #mutaiton rate
 
-def run(args):
-
+class World:
+    def __init__(self,world_size,startX,startY,foodCount,muta_rate):
+        self.world_size = world_size
+        self.startX =startX
+        self.startY = startY
+        self.foodCount = foodCount
+        self.mutation_rate = muta_rate
+        self.gene_length = math.floor(world_size*foodCount*1.5)#aka chromosome length
+        self.map = self.generateWorld()
     
+    #Generates world and places foods
+    def generateWorld(self):
+        N = self.world_size
+        world = np.zeros( shape=(N,N) )
+
+        food_positions = []
+
+        for _ in range(self.foodCount):
+            food = [random.randint(0,N-1),random.randint(0,N-1)]
+            while food in food_positions:
+                food = [random.randint(0,N-1),random.randint(0,N-1)]
+            food_positions.append(food)
+            world[food[0],food[1]] = 1
+
+        return world
+
+def run(args):
 
     startX = args.startx
     startY = args.starty
     foodCount = args.food_count
-    world = generateWorld(args.world_size)
 
-    gene_length = int(args.world_size*args.food_count*0.6)#aka chromosome length (0.6 is random)
+    world = World(args.world_size,args.startx,args.starty,args.food_count,args.mut_rate)
 
     #Generating the Population
-    population = []
-    for i in range(args.population_size):
-        population.append(generatePathRandom(gene_length))
+    population = Population(args.population_size,world.gene_length,True,[])
+
+    start_time = time.time()
+    elapsed_time = 0
     
-    population_finess_values = []
     total_fitness = 0
     max_fitness = 0
     max_fitness_index = 0
-    
-    for i in range(args.population_size):
-        fit = fitness(world,population[i],startX,startY,foodCount)
-        population_finess_values.append(fit)
+    i=0
+    for individual in population.individuals:
+        fit = fitnessFN(world,individual.path,startX,startY,foodCount)
+        individual.setFitness(fit)
         total_fitness += fit
         if fit > max_fitness : 
             max_fitness = fit
             max_fitness_index = i
+        i+=1
 
-    avg_fitness = total_fitness / args.population_size
+    best_individual = copy.copy(population.individuals[max_fitness_index])
+
+    avg_fitness = total_fitness / population.population_size
     
+
+    total_generations = args.max_generation_number
+    generation_counter = 0
+
+    while generation_counter < total_generations and best_individual.fitness < world.foodCount:
+        print("Generation : ",generation_counter)
+        print("Max fitness: ",best_individual.fitness)
+        print("Average fitness: ",avg_fitness)
+        population = evolve(world,population)
+        new_best_individual = population.getFittest()
+        avg_fitness = population.calculateAverageFitness()
+
+        if new_best_individual.fitness > best_individual.fitness:
+            generation_counter,best_individual = 0,new_best_individual
+        else:
+            generation_counter += 1
+        
 
     total_generations = 0
 
-    start_time = time.time()
-    elapsed_time = 0
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print("\n-------------------------------\nElapsed Time: ",elapsed_time," sec")
+    print("Population Size: ",args.population_size," World Size: ",args.world_size,"\nStart x, y:",args.startx,",",args.starty," Food Count: ",args.food_count,"\nMutation Rate: ",args.mut_rate)
 
-    while elapsed_time > args.duration :## or  avg_fitness >= 2 : #### or enough fit individiual found
-        elapsed_time = (time.time() - start_time)##this is as seconds
-        total_generations +=1
-        #new_population <- empty set
-        new_population = []
-        for i in range(args.population_size):
+    print("Result: ",best_individual.toString())
+    
+    matrix = traverse(best_individual.path,world)
+    draw_matrix(matrix,'title')
 
-           # x = fitness(ma)
 
-            if random.random() < args.mut_rate:
-                i=2
-              #  mutate(child)
+    
+    
+def traverse(path, world:World):
+    matrix = [[0 for a in range(world.world_size)] for b in range(world.world_size)]
+    x,y = world.startX,world.startY
 
-def generateWorld(N = 10,foodCount= 5):
+    eaten_foods = []
+    matrix[x][y] = 0.22
+    for i in range(len(path)):
+        x,y = proceed(x,y,path[i])
 
-    world = np.zeros( shape=(N,N) )
+        collision = checkCollision(world.map,world.world_size,eaten_foods,x,y) 
 
-    food_positions = []
+        if collision == 0:#no collision
+            matrix[x][y] = 0.55
 
-    for _ in range(foodCount):
-        food = [random.randint(0,N-1),random.randint(0,N-1)]
-        food_positions.append(food)
-        world[food[0],food[1]] = 1
+        elif collision > 0:#collided with food
+            None
+            #matrix[x][y] = 1
+                
+        else :# collision < 0 collided with wall
+            if x == world.world_size:
+                matrix[x-1][y] = 0.77
+            if y == world.world_size:
+                matrix[x][y-1] = 0.77
+            break
 
-    return world
+    x,y = world.startX,world.startY
+    matrix[x][y] = 0.22
+    for i in range(len(eaten_foods)):
+        matrix[eaten_foods[i][0]][eaten_foods[i][1]] = 1
+
+    return matrix
+
+def draw_matrix(matrix, title=''):
+    cmap = colors.ListedColormap(['white','black', 'red', 'green'])
+    bounds = [0, 0.2,0.5,0.7, 1]
+    norm = colors.BoundaryNorm(bounds, cmap.N)
+    plt.matshow(matrix, cmap=cmap, norm=norm, interpolation='none', vmin=0, vmax=4)
+    plt.title(title)
+    plt.xticks(range(len(matrix[0])))
+    plt.yticks(range(len(matrix[0])))
+    
+    plt.show()
 
 def generatePathRandom(gene_length):
     path = []
@@ -78,9 +156,6 @@ def generatePathRandom(gene_length):
         path.append( random.randint(1,4) )
 
     return path
-
-def mutate(child):
-    i = 2
 
 def proceed(posX,posY,direction):
         if direction == 1:#right
@@ -104,11 +179,11 @@ def checkCollision(world,world_size,eaten_foods,posX,posY):
     
     return 0
 
-def fitness(world,path,startX,startY,foodCount):
+def fitnessFN(world,path,startX,startY,foodCount):
     #Return fitness value for path parameter
     #min:0, max:2
     total_path = len(path)
-    world_size = len(world[0])
+    world_size = world.world_size
 
     posX = startX
     posY = startY   
@@ -122,7 +197,7 @@ def fitness(world,path,startX,startY,foodCount):
 
         posX,posY = proceed(posX,posY,path[i])
 
-        collision = checkCollision(world,world_size,eaten_foods,posX,posY) 
+        collision = checkCollision(world.map,world_size,eaten_foods,posX,posY) 
 
         if collision == 0:#no collision
             continue
@@ -131,32 +206,101 @@ def fitness(world,path,startX,startY,foodCount):
 
             if(len(eaten_foods) == foodCount):#eaten all food
                 #fitness = food_eaten/total_food_count + i/total_path !!! can it be enhanced ???
-                result = 1 + i/total_path
+                result = foodCount
                 break
                 
         else :# collision < 0 collided with wall ## min:0, max:2
-            result = len(eaten_foods)/total_food_count + i/total_path
+            result = len(eaten_foods)##/total_food_count + i/total_path - 1
             break
 
     if(result == -1):
-        result = len(eaten_foods)/total_food_count + 1
+        result = len(eaten_foods)##/total_food_count + 1
 
     return result
+
+def evolve(world :World, population:Population):
+
+    new_generation = Population(population.population_size,world.gene_length,False,[])
+    elitism_number = population.population_size//2
+
+    #Elitism
+    for _ in range(elitism_number):
+        fittest = population.getFittest()
+        new_generation.addIndividual(fittest)
+        population.removeIndividual(fittest)
+
+    #Crossover
+    for _ in range(elitism_number,population.population_size):
+        parent1 = selection(new_generation,world)
+        parent2 = selection(new_generation,world)
+        child = crossover(parent1,parent2)
+        child.setFitness(fitnessFN(world,child.path,world.startX,world.startY,world.foodCount))
+        new_generation.addIndividual(child)
+
+    #Mutation
+    for i in range(elitism_number,population.population_size):
+        mutation(new_generation.individuals[i],world.mutation_rate)
+    
+    return new_generation
+
+#select n number individual randomly and get fittest amoung them
+def selection(population:Population, world:World):
+    besafe=0
+    while besafe < 10000:
+        index  = randint(0,len(population.individuals)-1)
+        partner =  population.individuals[index]
+        r = random.randint(0,world.foodCount)#############BURAYA bi daha bak
+
+        if r < partner.fitness:
+            return partner
+
+        besafe+=1
+
+    index  = randint(0,len(population.individuals)-1)
+    partner =  population.individuals[index]
+    return partner
+   ## return Population(population.population_size,world.gene_length,False,sample(population.individuals,competitors_n)).getFittest()
+
+def crossover(parent1,parent2):
+
+    path_n = len(parent1.path)
+
+    mid = randint(0,path_n-1)  
+    child = Individual(path_n,False,[None for _ in range(path_n)])
+
+    for i in range(0, mid):
+            child.path[i] = parent1.path[i]
+
+    for i in range(mid, path_n):
+            child.path[i] = parent2.path[i]
+
+    return child
+
+def mutation(individual:Individual, mut_rate):
+    if random.random() < mut_rate:
+        mutate(individual.path)
+
+def mutate(path):
+    number_of_mutation = randint(0,len(path)//3)
+    for _ in range(number_of_mutation):
+        path[randint(0,len(path)-1)] = randint(1,4)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--world_size",type=int,default=11)
-    parser.add_argument("--food_count",type=int,default=5)
-    parser.add_argument("--population_size",type=int,default=100)
-    parser.add_argument('--mut_rate', type=float, default=0.02, help='Mutation rate')
+    parser.add_argument("--world_size",type=int,default=17)
+    parser.add_argument("--food_count",type=int,default=10)
+    parser.add_argument("--population_size",type=int,default=200)
+    parser.add_argument('--mut_rate', type=float, default=0.2, help='Mutation rate')
     parser.add_argument('--n_gen', type=int, default=20, help='Number of equal generations before stopping')
     parser.add_argument("--startx",type=int,default=5,help='start position x')
     parser.add_argument("--starty",type=int,default=5,help='start position y')
-    parser.add_argument("--duration",type=int,default=5,help='waiting time')
+    parser.add_argument("--duration",type=int,default=500,help='waiting time')
+    parser.add_argument("--max_generation_number",type=int,default=450,help='Max generation number for stop')
 
 
-    random.seed(datetime.now())
+    #random.seed(datetime.now())
 
     args = parser.parse_args()
 
